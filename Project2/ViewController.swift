f451403f8bb8a04069a1bd57286f467a329542c6
Lucas Maniero <lucasfomaniero@gmail.com>
2022-01-01 19:58:37 -0300

    Created the basic interface of the app.

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var flagsNames = [String]()
+    
+    let vStack: UIStackView = {
+        let sv = UIStackView()
+        sv.alignment = .center
+        sv.axis = .vertical
+        sv.distribution = .fillEqually
+        
+        sv.translatesAutoresizingMaskIntoConstraints = false
+        sv.spacing = 16
+        return sv
+    }()
+    
+    let button1: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named:"estonia"), for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 1
+        return button
+    }()
+    
+    let button2: UIButton = {
+        let button = UIButton()
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 1
+        button.setImage(UIImage(named:"france"), for: .normal)
+        return button
+    }()
+    
+    let button3: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(named:"germany"), for: .normal)
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 1
+        return button
+    }()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        setupConstraints()
+    }
+    
+   
+    
+    private func setupConstraints() {
+        view.addSubview(vStack)
+        vStack.addSubviews(button1, button2, button3)
+        let safeArea = view.safeAreaLayoutGuide
+        
+        NSLayoutConstraint.activate([
+            vStack.leftAnchor.constraint(equalTo: safeArea.leftAnchor, constant: 8),
+            vStack.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 8),
+            vStack.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -8),
+            vStack.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -8)
+        ])
     }
 
 
 }
 
+extension UIStackView {
+    func addSubviews(_ views:UIView...) {
+        for view in views {
+            self.addArrangedSubview(view)
+        }
+    }
+}
